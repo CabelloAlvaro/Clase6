8bcbe8d25dbe60d6c3b8f3410ae7646f9fa9512d
Alvaro Cabello <aacabello@uc.cl>
2022-09-22 18:43:19 -0300

    Repo ejemplo server express

---
+++ express.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    let objeto = { ruta: "raiz"}
+    res.send(objeto)
+})
+
+app.get('/user', (req, res) => {
+    let objeto = { ruta: "user"}
+    res.send(objeto)
+})
+
+const PORT = process.env.PORT || 8080
+
+const server = app.listen(8080, () => console.log('Server running'))
+server.on('error', error => console.log(`Error: ${error}`))
